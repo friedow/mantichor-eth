81327ae015c82174bb23b651f03df13b16dee931
Christian Friedow <christian@friedow.com>
2020-01-20 17:22:06 +0100

    Make HbsCompiler functions static

--- src/HbsCompiler.ts
+++ src/HbsCompiler.ts
@@ -3,28 +3,20 @@ import path from 'path';
 import Handlebars from 'handlebars';
 
 export default class HbsCompiler {
-
-    public contractPath: string;
-    public partialsPath: string;
-
-    constructor(contractPath: string, partialsPath: string) {
-        this.contractPath = contractPath;
-        this.partialsPath = partialsPath;
-
-        this.registerPartials();
-    }
-
-    private registerPartials(): void {
-        for (const partialFileName of fs.readdirSync(this.partialsPath)) {
-            const partialHbs = fs.readFileSync(path.resolve(this.partialsPath, partialFileName), "utf8");
+    
+    private static registerPartials(partialsPath: string): void {
+        for (const partialFileName of fs.readdirSync(partialsPath)) {
+            const partialHbs = fs.readFileSync(path.resolve(partialsPath, partialFileName), "utf8");
             const partialName = partialFileName.replace('.sol.hbs', '');
             Handlebars.registerPartial(partialName, partialHbs);
         }
     }
-
-    public compile(): string {
-        const contractHbs = fs.readFileSync(this.contractPath, "utf8");
+    
+    public static compile(contractPath: string, partialsPath: string): string {
+        HbsCompiler.registerPartials(partialsPath);
+        const contractHbs = fs.readFileSync(contractPath, "utf8");
         const contractTemplate = Handlebars.compile(contractHbs);
         return contractTemplate({});
     }
+
 }
